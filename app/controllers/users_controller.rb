47c4ece702b74d19841e6828fe766ed0196f4a6f
Kazuki Tsunoda <a2025kt@aiit.ac.jp>
2020-06-06 08:06:25 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,30 @@
 class UsersController < ApplicationController
-  def new
+  def show
+    @user = User.find(params[:id])
   end
+
+    # user だけだとローカル変数となる(このアクションの外側で使えない)
+    # @user はインスタンス変数、@@user はグローバル変数　=> グローバル変数は強力過ぎるので、極力つかわない方がいい
+
+  def new
+    @user = User.new
+  end
+  
+   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+      # 保存の成功をここで扱う。
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
